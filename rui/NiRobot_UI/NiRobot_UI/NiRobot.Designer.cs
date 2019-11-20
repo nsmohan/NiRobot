9be0113235a3319a34e5fc3c23742774a1020653
Nitin Mohan <nmohan101@gmail.com>
2019-11-19 21:55:00 -0500

    UI Updates Read RSXA File

--- rui/NiRobot_UI/NiRobot_UI/NiRobot.Designer.cs
+++ rui/NiRobot_UI/NiRobot_UI/NiRobot.Designer.cs
@@ -33,6 +33,14 @@
             this.ip_textbox = new System.Windows.Forms.TextBox();
             this.ip_disconnect_button = new System.Windows.Forms.Button();
             this.left_button = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.hwgbox = new System.Windows.Forms.GroupBox();
+            this.hw_list = new System.Windows.Forms.ListBox();
+            this.hw_mode_label = new System.Windows.Forms.Label();
+            this.hw_status_label = new System.Windows.Forms.Label();
+            this.hw_sim_mode = new System.Windows.Forms.Button();
+            this.hw_real_mode = new System.Windows.Forms.Button();
+            this.hwgbox.SuspendLayout();
             this.SuspendLayout();
             // 
             // ip_connect_button
@@ -73,7 +81,7 @@
             // 
             // left_button
             // 
-            this.left_button.Location = new System.Drawing.Point(201, 72);
+            this.left_button.Location = new System.Drawing.Point(116, 170);
             this.left_button.Name = "left_button";
             this.left_button.Size = new System.Drawing.Size(75, 23);
             this.left_button.TabIndex = 4;
@@ -81,11 +89,70 @@
             this.left_button.UseVisualStyleBackColor = true;
             this.left_button.Click += new System.EventHandler(this.left_button_Click);
             // 
+            // hwgbox
+            // 
+            this.hwgbox.Controls.Add(this.hw_real_mode);
+            this.hwgbox.Controls.Add(this.hw_sim_mode);
+            this.hwgbox.Controls.Add(this.hw_status_label);
+            this.hwgbox.Controls.Add(this.hw_mode_label);
+            this.hwgbox.Controls.Add(this.hw_list);
+            this.hwgbox.Location = new System.Drawing.Point(514, 9);
+            this.hwgbox.Name = "hwgbox";
+            this.hwgbox.Size = new System.Drawing.Size(274, 368);
+            this.hwgbox.TabIndex = 5;
+            this.hwgbox.TabStop = false;
+            this.hwgbox.Text = "Hardware";
+            // 
+            // hw_list
+            // 
+            this.hw_list.FormattingEnabled = true;
+            this.hw_list.Location = new System.Drawing.Point(6, 19);
+            this.hw_list.Name = "hw_list";
+            this.hw_list.Size = new System.Drawing.Size(135, 121);
+            this.hw_list.TabIndex = 6;
+            // 
+            // hw_mode_label
+            // 
+            this.hw_mode_label.AutoSize = true;
+            this.hw_mode_label.Location = new System.Drawing.Point(6, 161);
+            this.hw_mode_label.Name = "hw_mode_label";
+            this.hw_mode_label.Size = new System.Drawing.Size(37, 13);
+            this.hw_mode_label.TabIndex = 7;
+            this.hw_mode_label.Text = "Mode:";
+            // 
+            // hw_status_label
+            // 
+            this.hw_status_label.AutoSize = true;
+            this.hw_status_label.Location = new System.Drawing.Point(9, 196);
+            this.hw_status_label.Name = "hw_status_label";
+            this.hw_status_label.Size = new System.Drawing.Size(37, 13);
+            this.hw_status_label.TabIndex = 8;
+            this.hw_status_label.Text = "Status";
+            // 
+            // hw_sim_mode
+            // 
+            this.hw_sim_mode.Location = new System.Drawing.Point(50, 156);
+            this.hw_sim_mode.Name = "hw_sim_mode";
+            this.hw_sim_mode.Size = new System.Drawing.Size(75, 23);
+            this.hw_sim_mode.TabIndex = 9;
+            this.hw_sim_mode.Text = "Simulation";
+            this.hw_sim_mode.UseVisualStyleBackColor = true;
+            // 
+            // hw_real_mode
+            // 
+            this.hw_real_mode.Location = new System.Drawing.Point(131, 156);
+            this.hw_real_mode.Name = "hw_real_mode";
+            this.hw_real_mode.Size = new System.Drawing.Size(75, 23);
+            this.hw_real_mode.TabIndex = 10;
+            this.hw_real_mode.Text = "Real";
+            this.hw_real_mode.UseVisualStyleBackColor = true;
+            // 
             // NiRobot
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.hwgbox);
             this.Controls.Add(this.left_button);
             this.Controls.Add(this.ip_disconnect_button);
             this.Controls.Add(this.ip_textbox);
@@ -94,6 +161,8 @@
             this.Name = "NiRobot";
             this.Text = "NiRobot";
             this.Load += new System.EventHandler(this.NiRobot_Load);
+            this.hwgbox.ResumeLayout(false);
+            this.hwgbox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,6 +175,13 @@
         private System.Windows.Forms.TextBox ip_textbox;
         private System.Windows.Forms.Button ip_disconnect_button;
         private System.Windows.Forms.Button left_button;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox hwgbox;
+        private System.Windows.Forms.ListBox hw_list;
+        private System.Windows.Forms.Button hw_real_mode;
+        private System.Windows.Forms.Button hw_sim_mode;
+        private System.Windows.Forms.Label hw_status_label;
+        private System.Windows.Forms.Label hw_mode_label;
     }
 }
 
--- rui/NiRobot_UI/NiRobot_UI/NiRobot.cs
+++ rui/NiRobot_UI/NiRobot_UI/NiRobot.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,59 +9,132 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Renci.SshNet;
+using Newtonsoft.Json;
 
 namespace NiRobot_UI
 {
 
-    
+
     public partial class NiRobot : Form
     {
         /* Global Varibles */
-        SshClient nirobot;
+        SshClient nirobot_ssh;
+        ScpClient nirobot_scp;
+        string bin_path = "/home/nmohan/github/NiRobot/bin";
+        string rsxa = "/home/nmohan/github/NiRobot/config/RSXA.json";
+        string camera_horizontal_ct = "CAM_HRZN_MTR";
+        string camera_vertical_ct = "CAM_VERT_MTR";
+        string camera_control = "MTDR";
 
         public NiRobot()
         {
             InitializeComponent();
         }
 
+        public class RSXA_Settings
+        {
+            public IDictionary<string, bool> rsxa_settings = new Dictionary<string, bool>();
+            private string tmp_dir = @"C:\Windows\Temp\RSXA.json";
+
+            /* Constructor */
+            public RSXA_Settings(ScpClient nirobot_scp, string rsxa)
+            {
+                //Input     : NiRobot SCP Oboject and RSXA settings file
+                //Output    : N/A - Constructor
+                //Function  : Download the RSXA File from robot and read the settings
+
+                /* Download the file from the robot over via SCP */
+                using (Stream tmp_file = File.Create(tmp_dir))
+                {
+                    nirobot_scp.Download(rsxa, tmp_file);
+                }
+
+                /* Parse the contents of the file */
+                using (StreamReader rsxa_stream = new StreamReader(tmp_dir))
+                {
+                    dynamic rsxa_obj = JsonConvert.DeserializeObject(rsxa_stream.ReadToEnd());
+
+                    foreach (var hw in rsxa_obj.hw)
+                    {
+                        rsxa_settings.Add(new KeyValuePair<string, bool>((string)hw.hw_name, (bool)hw.hw_sim_mode));
+                    }
+                }
+            }
+        }
+
         private void NiRobot_Load(object sender, EventArgs e)
         {
+            /* Set Intial state of all elements when the page loads */
             ip_disconnect_button.Enabled = false;
         }
 
         private void ip_connect_button_Click(object sender, EventArgs e)
         {
-            string       ip_address = ip_textbox.Text;
+            //Input     : N/A - Button Click
+            //Output    : N/A - Button Click
+            //Function  : Create SSH and SCP Connection with robot
+
+            /* Initialize Varibles */
+            string ip_address = ip_textbox.Text;
             const int    ip_port    = 22;
             const string username   = "nmohan";
             const string password   = "nibot";
 
-            nirobot = new SshClient(ip_address, 22, username, password);
+            /*Initialize SSH and SCP Objects */
+            nirobot_ssh = new SshClient(ip_address, ip_port, username, password);
+            nirobot_scp = new ScpClient(ip_address, ip_port, username, password);
 
+            /* Try to Connect */
             try
             {
-                nirobot.Connect();
-                ip_connect_button.Enabled = false;
-                ip_disconnect_button.Enabled = true;
+                nirobot_ssh.Connect();
+                nirobot_scp.Connect();
             }
             catch 
             {
                 MessageBox.Show("Unable to connect! Please check IP Address!");
                 
             }
+
+            /* If we're connected to the robot we proceed */
+            if (nirobot_ssh.IsConnected && nirobot_scp.IsConnected)
+            {
+                /* Set Button States (Connect/Disconnect) */
+                ip_connect_button.Enabled = false;
+                ip_disconnect_button.Enabled = true;
+
+                /* Get File Settings */
+                RSXA_Settings robot_settings = new RSXA_Settings(nirobot_scp, rsxa);
+
+                /* Populate Hardware List box */
+                foreach (var hw_name in robot_settings.rsxa_settings.Keys)
+                {
+                    hw_list.Items.Add(hw_name);
+                }
+            }
                 
            }
 
         private void left_button_Click(object sender, EventArgs e)
         {
-            SshCommand command = nirobot.RunCommand("ls");
-            Console.WriteLine(command.Result);
+            //Input     : N/A - Button Click
+            //Output    : N/A - Button Click
+            //Function  : Move the robot to the left
 
+            int angle = 20;
+            string robot_command = $"{bin_path}{camera_control} --motor {camera_horizontal_ct} --angle {angle}";
+            SshCommand command = nirobot_ssh.RunCommand(robot_command);
+            Console.WriteLine(command.Result);
         }
 
         private void ip_disconnect_button_Click(object sender, EventArgs e)
         {
-            nirobot.Disconnect();
+            //Input     : N/A - Button Click
+            //Output    : N/A - Button Click
+            //Function  : Disconnect SSH and SCP connection
+
+            nirobot_ssh.Disconnect();
+            nirobot_scp.Disconnect();
             ip_connect_button.Enabled = true;
             ip_disconnect_button.Enabled = false;
         }
