a52fb9f34ca8e8ec10c43ca865e9cc258f1415ff
Mickey T Da Silva <mdasilva@callutheran.edu>
2020-02-07 05:41:10 -0500

    Doxygen commit; In order to use Doxygen, run the following command: 'doxygen Doxyfile' (requires doxygen installed)

---
+++ tools/doxygenMetadata.cpp
@@ -0,0 +1,17 @@
+/*!
+ * \author Nitin Mohan 
+ * \author Mickey T Da Silva
+ * \version 1.0
+ * \date 2020-02-06
+ * \warning Improper use of this code could result in a robot revolution
+ * \copyright Copyright 2020. NM Technologies
+ * \mainpage 
+ * \section intro_sec Introduction
+ * This code was developed to test doxygen
+ * \section compile_sec Compilation
+ * Here I describe how to comile this code with make
+ * \subsection Step_01 Test subsection
+ * This is almost identical to LaTeX
+ * \section link_sec Link to Doxygen Documentation
+ * <a href="http://www.doxygen.nl/manual/index.html">Link</a>
+ */
