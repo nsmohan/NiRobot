77e0fa02273b8ad6f72f9d06003895f8c1c2f533
Mickey T Da Silva <mdasilva@callutheran.edu>
2020-02-07 06:46:43 -0500

    Added Doxygen style comments for examples

--- tools/doxygenMetadata.cpp
+++ tools/doxygenMetadata.cpp
@@ -1,17 +1,23 @@
 /*!
- * \author Nitin Mohan 
- * \author Mickey T Da Silva
- * \version 1.0
- * \date 2020-02-06
- * \warning Improper use of this code could result in a robot revolution
- * \copyright Copyright 2020. NM Technologies
+ * \author          Nitin Mohan 
+ * \author          Mickey T Da Silva
+ * \version         0.1a
+ * \date            2020-02-06
+ *  
+ * \pre             Connect to Raspberry Pi3
+ * \warning         Improper use of this code could result in a robot revolution
+ * \bug             TEST BUG
+ * 
+ * \copyright       Copyright 2020. NM Technologies
+ * 
+ *  
  * \mainpage 
- * \section intro_sec Introduction
+ * \section         intro_sec Introduction
  * This code was developed to test doxygen
- * \section compile_sec Compilation
+ * \section         compile_sec Compilation
  * Here I describe how to comile this code with make
- * \subsection Step_01 Test subsection
+ * \subsection      Step_01 Test subsection
  * This is almost identical to LaTeX
- * \section link_sec Link to Doxygen Documentation
+ * \section         link_sec Link to Doxygen Documentation
  * <a href="http://www.doxygen.nl/manual/index.html">Link</a>
  */
